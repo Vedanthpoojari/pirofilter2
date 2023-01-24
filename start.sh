72efad8e6abafddea8e66f4f3b640b08c243b242
Vedanthpoojari <97805128+Vedanthpoojari@users.noreply.github.com>
2023-01-24 14:16:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Your_Repo_Name /LazyDeveloper
-cd /LazyDeveloper
+git clone https://github.com/Vedanthpoojari/pirofilter2 /pirofilter2
+cd /pirofilter2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python bot.py
